088a3753739166b525554ac39232fa898e61650b
amari <amari.lawal@gmail.com>
2025-03-23 22:04:05 +0000

    all services work perfecrtly. movies, music and it is accessible from the internet

--- CaesarAIConstants/__init__.py
+++ CaesarAIConstants/__init__.py
@@ -1,6 +1,6 @@
 import os
 class CaesarAIConstants:
     BASE_JACKETT_URL = "http://jackett:9117/api/v2.0/indexers/all/results/torznab"
-    JACKETT_API_KEY= os.environ["JACKETT_API_KEY"]
+    JACKETT_API_KEY= os.environ.get("JACKETT_API_KEY")
     ENDPOINT="tvsearch"
-    REALDEBRID_API_KEY= os.environ["REALDEBRID_API_KEY"]
+    REALDEBRID_API_KEY= os.environ.get("REALDEBRID_API_KEY")
